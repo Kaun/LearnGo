88496f8272e5e0a31f7b8a59d355b598fc658222
Aleksey Kaunnikov <kaunnikov@gmail.com>
2021-11-24 09:48:28 +0500

    AlgoritmBinary

---
+++ LearnAlgoritm/AlgoritmBinary.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func SearchBinary(slice []int, item int) (int, bool) {
+	h := len(slice) - 1
+	l := 0
+	var point int
+	for i := 0; i < len(slice); i++ {
+		point = (h + l) / 2
+		if l == h && item != slice[point] {
+			return 0, false
+		} else if item == slice[point] {
+			return point, true
+		} else if item > slice[point] {
+			l = point + 1
+		} else if item < slice[point] {
+			h = point - 1
+		}
+	}
+	return 0, false
+
+}
+
+func main() {
+
+	slc := []int{1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+
+	item := 20
+
+	fmt.Println(SearchBinary(slc, item))
+
+}
